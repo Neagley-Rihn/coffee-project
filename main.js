0f844e98f947ca7fe99cdaca8080890021bc23aa
JRihn32 <justin.rihn32@gmail.com>
2018-06-27 11:23:48 -0500

    working completely before styling

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
 "use strict"
-
+var coffees = [
+    {id: 1, name: 'Light City', roast: 'light'},
+    {id: 2, name: 'Half City', roast: 'light'},
+    {id: 3, name: 'Cinnamon', roast: 'light'},
+    {id: 4, name: 'City', roast: 'medium'},
+    {id: 5, name: 'American', roast: 'medium'},
+    {id: 6, name: 'Breakfast', roast: 'medium'},
+    {id: 7, name: 'High', roast: 'dark'},
+    {id: 8, name: 'Continental', roast: 'dark'},
+    {id: 9, name: 'New Orleans', roast: 'dark'},
+    {id: 10, name: 'European', roast: 'dark'},
+    {id: 11, name: 'Espresso', roast: 'dark'},
+    {id: 12, name: 'Viennese', roast: 'dark'},
+    {id: 13, name: 'Italian', roast: 'dark'},
+    {id: 14, name: 'French', roast: 'dark'},
+];
+let div = document.querySelector('#coffees');
+let submitButton = document.querySelector('#submit');
+let roastSelection = document.querySelector('#roast-selection');
 
 function renderCoffee(coffee) {
-    var html = '<div>';
+    let html = '<div>';
         html += '<div class="col-6 float-left"><span class="coffee_name">' + coffee.name + '</span>';
         html += '<span class="coffee_roast">' + coffee.roast + '</span></div>';
          html += '</div>';
@@ -10,25 +28,12 @@ function renderCoffee(coffee) {
 }
 
 function renderCoffees(coffees) {
-    var html = '';
-    for(var i = coffees.length - 1; i >= 0; i--) {
+    let html = '';
+    for(let i = (coffees.length - 1); i >= 0; i--) {
         html += renderCoffee(coffees[i]);
     }
     return html;
 }
-
-// function updateCoffees(e) {
-//     e.preventDefault(); // don't submit the form, we just want to update the data
-//     var selectedRoast = roastSelection.value;
-//     var filteredCoffees = [];
-//     coffees.forEach(function(coffee) {
-//         if (coffee.roast === selectedRoast) {
-//             filteredCoffees.push(coffee);
-//         }
-//     });
-//     div.innerHTML = renderCoffees(filteredCoffees);
-// }
-
 
 function updateCoffees(e) {
 
@@ -69,24 +74,8 @@ function updateCoffees(e) {
 
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
-var coffees = [
-    {id: 1, name: 'Light City', roast: 'light'},
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'},
-];
 
-var storedCoffees = JSON.parse(localStorage.getItem("coffees"));
+var storedCoffees = JSON.parse(sessionStorage.getItem("coffees"));
 
 function newCoffeeSubmition(){
     var coffeeName = document.getElementById("coffeeAddition").value;
@@ -98,24 +87,14 @@ function newCoffeeSubmition(){
         roast: coffeeRoast
     };
     coffees.push(newCoffee);
-    localStorage.setItem("coffees", JSON.stringify(coffees));
+    sessionStorage.setItem("coffees", JSON.stringify(coffees));
     document.getElementById("coffees").innerHTML = coffees;
 
 }
 
-// var allCoffees = [];
-//
-// // on page load, save all the coffees
-// allCoffees = coffees.forEach(function(coffee) {
-//     allCoffees.push(coffee);
-// console.log(allCoffees)
-// });
 
-var div = document.querySelector('#coffees');
-var submitButton = document.querySelector('#submit');
-var roastSelection = document.querySelector('#roast-selection');
 
-div.innerHTML = renderCoffees(storedCoffees);
+div.innerHTML = renderCoffees(coffees);
 
 roastSelection.addEventListener("change", updateCoffees);
 
@@ -128,19 +107,10 @@ submitButton.addEventListener('click', updateCoffees);
 
 document.getElementById("coffee_name").addEventListener("keyup", updateCoffees);
 
-// function searchCoffees(value){
-//     var filteredCoffees =[];
-//     for (var i=0; i < coffees.length; i++){
-//         if (coffees[i].name.toLowerCase().indexOf(value.toLowerCase())) {
-//             filteredCoffees.push(coffees[i]);
-//         }
-//     }
-//     div.innerHTML= renderCoffee(filteredCoffees);
-// }
-console.log(coffees);
-if (coffees.length < 14){
+
+if (storedCoffees.length < 15){
     div.innerHTML = renderCoffees(coffees);
 }else{
     div.innerHTML = renderCoffees(storedCoffees);
 }
-console.log(storedCoffees);
+
